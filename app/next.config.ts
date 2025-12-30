9515ab572305b3f0534a29fcdb5b1dadb7b47963
a19hu <ashutoshkumar1jkl56@gmail.com>
2025-12-30 13:07:04 +0530

    fix pino pretty

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.externals.push({
+      "pino-pretty": "commonjs pino-pretty",
+      lokijs: "commonjs lokijs",
+      encoding: "commonjs encoding",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
